03771d69464e29c0548db44dd8896a7ed7f93484
Lailson Nogueira <lailson.lima@gmail.com>
2023-11-08 21:28:23 -0300

    descomentando os testes

--- src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
+++ src/test/java/br/ce/wcaquino/taskbackend/utils/DateUtilsTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 
 public class DateUtilsTest {
 	
-	/*@Test
+	@Test
 	public void deveRetornarTrueParaDatasFuturas() {
 		LocalDate date = LocalDate.of(2030, 01, 01);
 		Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
@@ -23,6 +23,6 @@ public class DateUtilsTest {
 	public void deveRetornarTrueParaDataAtual() {
 		LocalDate date = LocalDate.now();
 		Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
-	}*/
+	}
 
 }
